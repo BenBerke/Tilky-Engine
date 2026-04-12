0c4a20f5220867bba28d48072ac84f26266725c2
BenBerke <berkememioglu@gmail.com>
2026-04-13 00:08:36 +0300

    Shader class

---
+++ Headers/Renderer/Shader.h
@@ -0,0 +1,96 @@
+//
+// Created by berke on 4/12/2026.
+//
+
+#ifndef WOLFY_ENGINE_SHADER_H
+#define WOLFY_ENGINE_SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <SDL3/SDL_log.h>
+
+class Shader {
+    public:
+    unsigned int ID{};
+    Shader(const char *vertexShaderPath, const char *fragmentShaderPath) {
+        std::string vertexShaderSourceCode;
+        std::string fragmentShaderSourceCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+
+        vShaderFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+        fShaderFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+        try {
+            vShaderFile.open(vertexShaderPath);
+            fShaderFile.open(fragmentShaderPath);
+            std::stringstream vShaderStream, fShaderStream;
+
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            vShaderFile.close();
+            fShaderFile.close();
+
+            vertexShaderSourceCode = vShaderStream.str();
+            fragmentShaderSourceCode = fShaderStream.str();
+        }
+        catch (std::ifstream::failure& e) {
+            SDL_Log("FILE ERROR: %s", e.what());
+            return;
+        }
+        const char* vShaderCode = vertexShaderSourceCode.c_str();
+        const char* fShaderCode = fragmentShaderSourceCode.c_str();
+
+        unsigned int vertex, fragment;
+        int success;
+        char infoLog[512];
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            SDL_Log("VERTEX ERROR: %s", infoLog);
+        }
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            SDL_Log("FRAGMENT ERROR: %s", infoLog);
+        }
+
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            SDL_Log("PROGRAM ERROR: %s", infoLog);
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    void use() const {
+        glUseProgram(ID);
+    }
+
+    void setBool(const std::string &name, const bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), static_cast<int>(value));
+    }
+    void setInt(const std::string &name, const int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setFloat(const std::string &name, const float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+};
+
+#endif //WOLFY_ENGINE_SHADER_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+#include <filesystem>
 #include <glad/glad.h>
 #include <SDL3/SDL_video.h>
 
 #include "Headers/Engine/InputManager.h"
 #include "Headers/Engine/GameTime.h"
 #include "Headers/Renderer/Renderer.h"
+#include "Headers/Renderer/Shader.h"
 
-#define SCREEN_WIDTH 960
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 960
 
 int main() {
     Renderer::Initialize();
@@ -16,68 +18,45 @@ int main() {
     }
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.5f,  0.5f, 0.0f,
+    constexpr float vertices[] = {
+        // positions          // colours
+        -1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   // top left
+        -1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   // bottom left
+         1.0f,  1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   // top right
+         1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 0.0f    // bottom right
+    };
+
+    const unsigned int indices[] = {
+        0, 1, 2,
+        1, 3, 2
     };
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    const char *vertexShaderSource = "#version 330 core \n"
-                                     "layout (location = 0) in vec3 aPos;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "gl_Position = vec4(aPos, 1.0);"
-                                     "}\n";
-    const unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        SDL_Log("Vertex shader compilation failed: %s", infoLog);
+    const Shader shader = Shader("../Shaders/vertex.vs", "../Shaders/frag.fs");
+    if (shader.ID == 0) {
+        SDL_Log("Shader creation failed");
+        return -1;
     }
 
-    const char *fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "FragColor = vec4(0.2f, 0.3f, 1.0f, 1.0f);\n"
-                                       "}\n";
-    const unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        SDL_Log("Fragment shader compilation failed: %s", infoLog);
-    }
-
-    const unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        SDL_Log("Program linking failed: %s", infoLog);
-    }
-    glUseProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     bool running = true;
     while (running) {
@@ -86,13 +65,16 @@ int main() {
 
         running = !InputManager::GetKeyDown(SDL_SCANCODE_ESCAPE);
 
+        shader.use();
         glClearColor(.2f, .3f, .3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         SDL_GL_SwapWindow(Renderer::window);
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 }
